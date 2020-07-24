a85c1f0ae035adc301bfc7ba003043e1674761ac
Kevin Miller <millerk@reninc.com>
2020-07-23 20:56:24 -0400

    reforamtting

--- examples/Inspection/Program.cs
+++ examples/Inspection/Program.cs
@@ -13,30 +13,27 @@ namespace Inspection
 
             var fparameter = Expression.Parameter(typeof(string), "firstName");
             var lparameter = Expression.Parameter(typeof(string), "lastName");
-            var aparameter = Expression.Parameter(typeof(int), "age");
-
 
             var fullNameVariable = Expression.Variable(typeof(string), "fullName");
+
             var concatMethodInfo = typeof(string).GetMethod("Concat", new[] { typeof(string), typeof(string) });
 
             var concatFNameWithSpace = Expression.Call(concatMethodInfo, new Expression[] { fparameter, Expression.Constant(" ") });
-
             var expressionAssign = Expression.Assign(fullNameVariable, concatFNameWithSpace);
-
             var concatLName = Expression.Call(concatMethodInfo, new Expression[] { fullNameVariable, lparameter });
 
-            var expressionBody = Expression.Block(new ParameterExpression[] { fullNameVariable }, concatFNameWithSpace, expressionAssign, concatLName);
+            var expressionBody = Expression.Block(new ParameterExpression[] { fullNameVariable }, expressionAssign, concatLName);
 
-            var lamba = Expression.Lambda(expressionBody, new[] { fparameter, lparameter, aparameter });
+            var lamba = Expression.Lambda(expressionBody, new[] { fparameter, lparameter });
+            var cLambda = (Func<string, string, string>)lamba.Compile();
 
-            var cLambda = (Func<string, string, int, string>)lamba.Compile();
-
-            var result = cLambda("Kevin", "Miller", 34);
 
             var placedExpression = (BlockExpression)lamba.Body;
-
             placedExpression.Expressions.ToList().ForEach(Console.WriteLine);
 
+            Console.WriteLine();
+
+            var result = cLambda("Kevin", "Miller");
             Console.WriteLine(result);
         }
 
